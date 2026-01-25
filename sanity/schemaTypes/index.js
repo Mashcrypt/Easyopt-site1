a262cc32137aa3ee6f3abc91d58ff259dfbd3a92
Mashcrypt <109661499+Mashcrypt@users.noreply.github.com>
2026-01-26 00:12:05 +0200

    Update index.js

--- sanity/schemaTypes/index.js
+++ sanity/schemaTypes/index.js
@@ -2,15 +2,11 @@ import job from './job'
 import university from './university'
 import bursary from './bursary'
 import cvTip from './cvTip'
-import recruiter from './recruiter'
-import company from './company'
 
 export const schemaTypes = [
   job,
   university,
   bursary,
-  cvTip,
-  recruiter,
-  company
+  cvTip
 ]
 
